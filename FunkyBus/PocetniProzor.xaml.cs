85345fa011bf31b7dd0681934bf04bcc72daf9ed
MatejBuric <mburic@pmfst.hr>
2023-01-12 13:49:49 +0100

    Korisničko Sučelje
    - Izbrisan početni "MainWindow" prozor
    - Dodan novi prozor "PocetniProzor"
    - "PocetniProzor" postavljen kao početni

--- FunkyBus/PocetniProzor.xaml.cs
+++ FunkyBus/PocetniProzor.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FunkyBus
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PocetniProzor.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PocetniProzor : Window
     {
-        public MainWindow()
+        public PocetniProzor()
         {
             InitializeComponent();
         }
